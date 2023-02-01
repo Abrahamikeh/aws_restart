261b559f05231772aa6b46e37ac7161d325142a9
Abrahamikeh <ikehabrahamikeh@gmail.com>
2023-02-01 10:55:48 +0100

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+print("Hello, World")
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
---
+++ string-data-type.py
@@ -0,0 +1,12 @@
+myString = "This is a string."
+print(myString)
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("Abrahan ikeh ")
+print(name)
+color = input("White ")
+animal = input("Goat  ")
+print("{}, you like a {} {}!".format(name,color,animal))
